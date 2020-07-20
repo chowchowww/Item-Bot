722fe17a1d4cf198ac919de4eb304d4e9414ba39
Chow <chaulea@gmail.com>
2020-07-20 20:30:00 +0700

    day #1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client } = require('discord.js');
 const client = new Client();
-const botConfig = require('./botConfig.json');
+const botConfig = require('./non_upload/botConfig.json');
 const mongoose = require('mongoose');
 const itemModel = require('./models/item');
 const fetch = require('node-fetch');
@@ -15,7 +15,6 @@ mongoose.connect(botConfig.MONGODB_URL, {
 mongoose.connection.on('connected', () => {
 	console.log('Mongoose is connected !');
 });
-
 
 client.once('ready', () => {
 	console.log(`${client.user.username} is online !`);
